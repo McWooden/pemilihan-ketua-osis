4914e7c0fbd157aae83453f31e7da8e7dce4794d
McWooden <garryf7887@gmail.com>
2023-12-19 22:16:04 +0700

    fix path

--- src/components/Base/dashboard/Hero.js
+++ src/components/Base/dashboard/Hero.js
@@ -1,4 +1,4 @@
-import SearchAndPlusButton from "../../utilsComponent/SearchAndPlusButton";
+import SearchAndPlusButton from "../../Utils/SearchAndPlusButton";
 
 export default function Hero() {
     return <div className="hero bg-base-200 rounded-lg py-4">
