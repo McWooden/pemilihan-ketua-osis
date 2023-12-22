816812a9c36c622640e596c2e2e57e1e82f9514c
McWooden <garryf7887@gmail.com>
2023-12-22 18:36:04 +0700

    react dropdown validation file

--- src/components/Base/form/Form.js
+++ src/components/Base/form/Form.js
@@ -1,174 +1,235 @@
+import { useCallback, useState } from "react"
+import { useDropzone } from "react-dropzone"
+
 export default function Form() {
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log('bjr');
+    const [nik, setNik] = useState(' ')
+    const [nama, setNama] = useState('')
+    const [tempatLahir, setTempatLahir] = useState('')
+    const [tanggalLahir, setTanggalLahir] = useState('')
+    const [jenisKelamin, setJenisKelamin] = useState('-')
+    const [golonganDarah, setGolonganDarah] = useState('-')
+    const [alamat, setAlamat] = useState('')
+    const [rt, setRt] = useState('')
+    const [rw, setRw] = useState('')
+    const [kelurahanDesa, setKelurahanDesa] = useState('')
+    const [kecamatan, setKecamatan] = useState('')
+    const [agama, setAgama] = useState('-')
+    const [pekerjaan, setPekerjaan] = useState('')
+    const [kewarganegaraan, setKewarganegaraan] = useState('-')
+
+    const onDrop = useCallback(acceptFiles => {
+        console.log(acceptFiles);
+    }, [])
+
+    const {getRootProps, getInputProps, isDragActive, isDragReject, isDragAccept} = useDropzone({
+        onDrop,
+        accept: {
+            'image/jpeg': ['.jpg'],
+            'application/pdf': ['.pdf']
+        },
+
+    })
+
+    async function handleSubmit(e) {
+        e.preventDefault()
     }
-    return (
-        <form onSubmit={handleSubmit} className="flex flex-col h-full gap-2 items-center py-4">
-            <label className="form-control w-full max-w-lg">
+
+    return <form className="flex flex-col h-full gap-2 items-center py-4" onSubmit={handleSubmit} {...getRootProps()}>
+        <label className="form-control w-full max-w-lg">
+            <div className="label">
+                <span className="label-text">Nomor Induk Kependudukan</span>
+            </div>
+            <input
+                value={nik}
+                onChange={(e) => setNik(e.target.value)}
+                type="number"
+                placeholder="Ketik 16 digit disini"
+                className="input input-bordered w-full"
+                max={16} min={16}
+            />
+        </label>
+        <label className="form-control w-full max-w-lg">
+            <div className="label">
+                <span className="label-text">Nama</span>
+            </div>
+            <input
+                value={nama}
+                onChange={(e) => setNama(e.target.value)}
+                type="text"
+                placeholder="Ketik disini"
+                className="input input-bordered w-full"
+            />
+        </label>
+        <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
+            <label className="form-control w-full">
                 <div className="label">
-                    <span className="label-text">Nomor Induk Kependudukan</span>
+                    <span className="label-text">Tempat Lahir</span>
                 </div>
                 <input
-                    type="number"
-                    placeholder="Ketik 16 digit disini"
-                    className="input input-bordered w-full"
-                    max={16} min={16}
-                />
-            </label>
-            <label className="form-control w-full max-w-lg">
-                <div className="label">
-                    <span className="label-text">Nama</span>
-                </div>
-                <input
+                    value={tempatLahir}
+                    onChange={(e) => setTempatLahir(e.target.value)}
                     type="text"
                     placeholder="Ketik disini"
                     className="input input-bordered w-full"
                 />
             </label>
-            <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
-                <label className="form-control w-full">
-                    <div className="label">
-                        <span className="label-text">Tempat Lahir</span>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="Ketik disini"
-                        className="input input-bordered w-full"
-                    />
-                </label>
-                <label className="form-control w-full">
-                    <div className="label">
-                        <span className="label-text">Tanggal Lahir</span>
-                    </div>
-                    <input
-                        type="date"
-                        className="input input-bordered w-full"
-                    />
-                </label>
-            </div>
-            <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
-                <label className="form-control w-full">
-                    <div className="label">
-                        <span className="label-text">Jenis Kelamin</span>
-                    </div>
-                    <select className="select select-bordered" defaultValue='-'>
-                        <option disabled value='-'>Pilih satu</option>
-                        <option value='Laki-laki'>Laki-laki</option>
-                        <option value='Perempuan'>Perempuan</option>
-                    </select>
-                </label>
-                <label className="form-control w-full">
-                    <div className="label">
-                        <span className="label-text">Gol Darah</span>
-                    </div>
-                    <select className="select select-bordered" defaultValue='-'>
-                        <option disabled value='-'>Pilih satu</option>
-                        <option value='A'>A</option>
-                        <option value='B'>B</option>
-                        <option value='AB'>AB</option>
-                        <option value='O'>O</option>
-                    </select>
-                </label>
-            </div>
-            <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
-                <label className="form-control w-full flex-1">
-                    <div className="label">
-                        <span className="label-text">Alamat</span>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="Ketik disini"
-                        className="input input-bordered w-full"
-                    />
-                </label>
-                <div className="flex gap-2 max-w-lg flex-1">
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text">RT</span>
-                        </div>
-                        <input
-                            type="number"
-                            placeholder="Ketik disini"
-                            className="input input-bordered w-full"
-                        />
-                    </label>
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text">RW</span>
-                        </div>
-                        <input
-                            type="number"
-                            placeholder="Ketik disini"
-                            className="input input-bordered w-full"
-                        />
-                    </label>
-                </div>
-            </div>
-            <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
-                <label className="form-control w-full">
-                    <div className="label">
-                        <span className="label-text">Kel/Desa</span>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="Ketik disini"
-                        className="input input-bordered w-full"
-                    />
-                </label>
-                <label className="form-control w-full">
-                    <div className="label">
-                        <span className="label-text">Kecamatan</span>
-                    </div>
-                    <input
-                        type="text"
-                        placeholder="Ketik disini"
-                        className="input input-bordered w-full"
-                    />
-                </label>
-            </div>
-            <label className="form-control w-full max-w-lg">
+            <label className="form-control w-full">
                 <div className="label">
-                    <span className="label-text">Agama</span>
-                </div>
-                <select className="select select-bordered" defaultValue='-'>
-                    <option disabled value='-'>Pilih satu</option>
-                    <option value='Islam<'>Islam</option>
-                    <option value='Kristen'>Kristen</option>
-                    <option value='Katolik'>Katolik</option>
-                    <option value='Hindu<'>Hindu</option>
-                    <option value='Buddha'>Buddha</option>
-                    <option value='Khonghucu'>Khonghucu</option>
-                </select>
-            </label>
-            <label className="form-control w-full max-w-lg">
-                <div className="label">
-                    <span className="label-text">Pekerjaan</span>
+                    <span className="label-text">Tanggal Lahir</span>
                 </div>
                 <input
+                    value={tanggalLahir}
+                    onChange={(e) => setTanggalLahir(e.target.value)}
+                    type="date"
+                    className="input input-bordered w-full"
+                />
+            </label>
+        </div>
+        <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
+            <label className="form-control w-full">
+                <div className="label">
+                    <span className="label-text">Jenis Kelamin</span>
+                </div>
+                <select className="select select-bordered" value={jenisKelamin} onChange={(e) => setJenisKelamin(e.target.value)}>
+                    <option disabled value='-'>Pilih satu</option>
+                    <option value='Laki-laki'>Laki-laki</option>
+                    <option value='Perempuan'>Perempuan</option>
+                </select>
+            </label>
+            <label className="form-control w-full">
+                <div className="label">
+                    <span className="label-text">Gol Darah</span>
+                </div>
+                <select className="select select-bordered" value={golonganDarah} onChange={(e) => setGolonganDarah(e.target.value)}>
+                    <option disabled value='-'>Pilih satu</option>
+                    <option value='A'>A</option>
+                    <option value='B'>B</option>
+                    <option value='AB'>AB</option>
+                    <option value='O'>O</option>
+                </select>
+            </label>
+        </div>
+        <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
+            <label className="form-control w-full flex-1">
+                <div className="label">
+                    <span className="label-text">Alamat</span>
+                </div>
+                <input
+                    value={alamat}
+                    onChange={(e) => setAlamat(e.target.value)}
                     type="text"
                     placeholder="Ketik disini"
                     className="input input-bordered w-full"
                 />
             </label>
-            <label className="form-control w-full max-w-lg">
+            <div className="flex gap-2 max-w-lg flex-1">
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text">RT</span>
+                    </div>
+                    <input
+                        value={rt}
+                        onChange={(e) => setRt(e.target.value)}
+                        type="number"
+                        placeholder="Ketik disini"
+                        className="input input-bordered w-full"
+                    />
+                </label>
+                <label className="form-control w-full">
+                    <div className="label">
+                        <span className="label-text">RW</span>
+                    </div>
+                    <input
+                        value={rw}
+                        onChange={(e) => setRw(e.target.value)}
+                        type="number"
+                        placeholder="Ketik disini"
+                        className="input input-bordered w-full"
+                    />
+                </label>
+            </div>
+        </div>
+        <div className="flex gap-2 max-w-lg w-full max-w-lg sm:flex-row flex-col">
+            <label className="form-control w-full">
                 <div className="label">
-                    <span className="label-text">Kewarganegaraan</span>
+                    <span className="label-text">Kel/Desa</span>
                 </div>
-                <select className="select select-bordered" defaultValue='-'>
-                    <option disabled value='-'>Pilih satu</option>
-                    <option value='Islam<'>WNI</option>
-                    <option value='Kristen'>WNA</option>
-                    <option value='Katolik'>ITAP</option>
-                </select>
+                <input
+                    value={kelurahanDesa}
+                    onChange={(e) => setKelurahanDesa(e.target.value)}
+                    type="text"
+                    placeholder="Ketik disini"
+                    className="input input-bordered w-full"
+                />
             </label>
-            <label className="form-control w-full max-w-lg">
+            <label className="form-control w-full">
                 <div className="label">
-                    <span className="label-text">Foto KTP</span>
+                    <span className="label-text">Kecamatan</span>
                 </div>
-                <input type="file" className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-neutral text-neutral file:text-neutral-content"/>
+                <input
+                    value={kecamatan}
+                    onChange={(e) => setKecamatan(e.target.value)}
+                    type="text"
+                    placeholder="Ketik disini"
+                    className="input input-bordered w-full"
+                />
             </label>
-            <button className="btn btn-accent w-full max-w-md mt-4">Tambah</button>
-        </form>
-    );
+        </div>
+        <label className="form-control w-full max-w-lg">
+            <div className="label">
+                <span className="label-text">Agama</span>
+            </div>
+            <select className="select select-bordered" value={agama} onChange={(e) => setAgama(e.target.value)}>
+                <option disabled value='-'>Pilih satu</option>
+                <option value='Islam<'>Islam</option>
+                <option value='Kristen'>Kristen</option>
+                <option value='Katolik'>Katolik</option>
+                <option value='Hindu<'>Hindu</option>
+                <option value='Buddha'>Buddha</option>
+                <option value='Khonghucu'>Khonghucu</option>
+            </select>
+        </label>
+        <label className="form-control w-full max-w-lg">
+            <div className="label">
+                <span className="label-text">Pekerjaan</span>
+            </div>
+            <input
+                value={pekerjaan}
+                onChange={(e) => setPekerjaan(e.target.value)}
+                type="text"
+                placeholder="Ketik disini"
+                className="input input-bordered w-full"
+            />
+        </label>
+        <label className="form-control w-full max-w-lg">
+            <div className="label">
+                <span className="label-text">Kewarganegaraan</span>
+            </div>
+            <select className="select select-bordered" value={kewarganegaraan} onChange={(e) => setKewarganegaraan(e.target.value)}>
+                <option disabled value='-'>Pilih satu</option>
+                <option value='Islam<'>WNI</option>
+                <option value='Kristen'>WNA</option>
+                <option value='Katolik'>ITAP</option>
+            </select>
+        </label>
+        <div className="w-full max-w-lg">
+            <div className="label">
+                <span className="label-text">Foto KTP</span>
+            </div>
+            <input type="file" className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-neutral text-neutral file:text-neutral-content cursor-pointer" accept="image/jpg,.pdf" {...getInputProps()} style={{display: 'block'}}/>
+            <div className="label">
+                <span className="label-text">klik atau jatuhkan disini</span>
+                <span className="label-text-alt">pdf dan jpg/jpeg</span>
+            </div>
+            {isDragActive && 
+                <div className={`absolute top-0 bottom-0 right-0 left-0 ${isDragAccept && 'bg-neutral/25 text-neutral'} ${isDragReject && 'bg-error/25 text-error-content'} z-[2] text-xl p-4`}>
+                    <div className={`border border-dashed ${isDragAccept && 'border-neutral'} ${isDragReject && 'border-error'} border-8 w-full h-full grid place-items-center`}>
+                        {isDragAccept && <p>File diterima</p>}
+                        {isDragReject && <p>Hanya menerima pdf dan jpg</p>}
+                    </div>
+                </div>
+            }
+        </div>
+        <button className="btn btn-accent w-full max-w-md mt-4">Tambah</button>
+    </form>
 }
