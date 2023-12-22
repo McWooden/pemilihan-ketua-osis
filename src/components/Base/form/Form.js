b3180d1b98d3cfeef326b4c81aa5d6788fce8e1b
McWooden <garryf7887@gmail.com>
2023-12-22 23:31:21 +0700

    sekarang jam setengah 12 malam, dan dari tadi maghrib nyari kenapa gamau di upload

--- src/components/Base/form/Form.js
+++ src/components/Base/form/Form.js
@@ -1,6 +1,10 @@
 import { useCallback, useState } from "react"
 import { useDropzone } from "react-dropzone"
+import supabase from "../../../config/supabaseClient"
+import AlertError from "../../Utils/AlertError"
 
+// preveew state
+// next finish this
 export default function Form() {
     const [nik, setNik] = useState(' ')
     const [nama, setNama] = useState('')
@@ -14,27 +18,59 @@ export default function Form() {
     const [kelurahanDesa, setKelurahanDesa] = useState('')
     const [kecamatan, setKecamatan] = useState('')
     const [agama, setAgama] = useState('-')
+    const [statusPerkawinan, setStatusPerkawinan] = useState('-')
     const [pekerjaan, setPekerjaan] = useState('')
     const [kewarganegaraan, setKewarganegaraan] = useState('-')
+    const [file, setFile] = useState([])
+
+    const [isloading, setIsLoading] = useState(false)
+    const [errorFileUpload, setErrorFileUpload] = useState(false)
+    const [errorInsertRow, setErrorInsertRow] = useState(false)
 
     const onDrop = useCallback(acceptFiles => {
-        console.log(acceptFiles);
+        console.log(acceptFiles && acceptFiles[0]);
+        setFile(acceptFiles[0] && acceptFiles[0])
     }, [])
 
     const {getRootProps, getInputProps, isDragActive, isDragReject, isDragAccept} = useDropzone({
         onDrop,
         accept: {
-            'image/jpeg': ['.jpg'],
+            'image/jpeg': ['.jpg', '.jpeg'],
             'application/pdf': ['.pdf']
         },
-
     })
 
     async function handleSubmit(e) {
         e.preventDefault()
+        setIsLoading(true)
+        setErrorFileUpload(false)
+        setErrorInsertRow(false)
+        let dataToSend = {
+            nik, nama,
+            tempatLahir, tanggalLahir,
+            jenisKelamin, golonganDarah,
+            alamat, rt, rw, kelurahanDesa, kecamatan,
+            agama, statusPerkawinan, pekerjaan, kewarganegaraan,
+            pathFileKtp: nik, typeFileKtp: 'image'
+        }
+        console.log('FILE', file)
+        try {
+            const {data, error} = await supabase.storage.from('fotoKtp').upload(nik, file)
+            console.log('error upload', error);
+            if (error) throw new Error(error)
+            dataToSend.pathFileKtp = data.path
+            setFile(null)
+            setIsLoading(false)
+        } catch (error) {
+            console.log(error);
+            setErrorFileUpload(true)
+            setIsLoading(false)
+        }
     }
 
-    return <form className="flex flex-col h-full gap-2 items-center py-4" onSubmit={handleSubmit} {...getRootProps()}>
+    return <form className="flex flex-col h-full gap-2 items-center py-4" onSubmit={handleSubmit}>
+        {errorFileUpload && <AlertError text="Error saat mengunggah file"/>}
+        {errorInsertRow && <AlertError text="Error saat menambahkan data"/>}
         <label className="form-control w-full max-w-lg">
             <div className="label">
                 <span className="label-text">Nomor Induk Kependudukan</span>
@@ -45,7 +81,8 @@ export default function Form() {
                 type="number"
                 placeholder="Ketik 16 digit disini"
                 className="input input-bordered w-full"
-                max={16} min={16}
+                // max={16} min={16}
+                required
             />
         </label>
         <label className="form-control w-full max-w-lg">
@@ -191,6 +228,16 @@ export default function Form() {
         </label>
         <label className="form-control w-full max-w-lg">
             <div className="label">
+                <span className="label-text">Status perkawinan</span>
+            </div>
+            <select className="select select-bordered" value={statusPerkawinan} onChange={(e) => setStatusPerkawinan(e.target.value)}>
+                <option disabled value='-'>Pilih satu</option>
+                <option value='Islam<'>Belum Kawin</option>
+                <option value='Kristen'>Kawin</option>
+            </select>
+        </label>
+        <label className="form-control w-full max-w-lg">
+            <div className="label">
                 <span className="label-text">Pekerjaan</span>
             </div>
             <input
@@ -212,24 +259,29 @@ export default function Form() {
                 <option value='Katolik'>ITAP</option>
             </select>
         </label>
-        <div className="w-full max-w-lg">
+        <div className="w-full max-w-lg relative" {...getRootProps()}>
             <div className="label">
                 <span className="label-text">Foto KTP</span>
             </div>
-            <input type="file" className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-neutral text-neutral file:text-neutral-content cursor-pointer" accept="image/jpg,.pdf" {...getInputProps()} style={{display: 'block'}}/>
+            <input type="file" className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-neutral text-neutral file:text-neutral-content cursor-pointer" accept="image/jpg,.pdf" {...getInputProps()} style={{display: 'block'}}
+                onChange={(x) => setFile(x)}
+            />
             <div className="label">
                 <span className="label-text">klik atau jatuhkan disini</span>
                 <span className="label-text-alt">pdf dan jpg/jpeg</span>
             </div>
             {isDragActive && 
-                <div className={`absolute top-0 bottom-0 right-0 left-0 ${isDragAccept && 'bg-neutral/25 text-neutral'} ${isDragReject && 'bg-error/25 text-error-content'} z-[2] text-xl p-4`}>
-                    <div className={`border border-dashed ${isDragAccept && 'border-neutral'} ${isDragReject && 'border-error'} border-8 w-full h-full grid place-items-center`}>
+                <div className={`absolute top-0 bottom-0 right-0 left-0 ${isDragAccept && 'bg-neutral/75 text-neutral-content'} ${isDragReject && 'bg-error/75 text-error-content'} z-[2] text-xl p-4`}>
+                    <div className={`border border-dashed ${isDragAccept && 'border-neutral-content'} ${isDragReject && 'border-error-content'} border-8 w-full h-full grid place-items-center`}>
                         {isDragAccept && <p>File diterima</p>}
                         {isDragReject && <p>Hanya menerima pdf dan jpg</p>}
                     </div>
                 </div>
             }
         </div>
-        <button className="btn btn-accent w-full max-w-md mt-4">Tambah</button>
+        <div className="w-full max-w-lg flex flex-col">
+            {/* {files.map((file, i) => <img src={URL.createObjectURL(file)} alt="foto ktp pengguna" className="w-full" key={i}/>)} */}
+        </div>
+        {isloading ? <span className='btn btn-accent w-full max-w-md mt-4'>Loading</span> : <button className="btn btn-accent w-full max-w-md mt-4">Tambah</button>}
     </form>
 }
