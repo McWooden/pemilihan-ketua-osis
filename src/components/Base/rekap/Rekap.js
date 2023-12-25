b628af3a5346b3c779a7908451d477a262b80211
McWooden <garryf7887@gmail.com>
2023-12-25 21:23:12 +0700

    download as excel

--- src/components/Base/rekap/Rekap.js
+++ src/components/Base/rekap/Rekap.js
@@ -8,6 +8,9 @@ import AlertError from "../../Utils/AlertError";
 import TableData from "./TableData";
 import { HiRefresh } from "react-icons/hi";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { PiMicrosoftExcelLogo } from "react-icons/pi";
+import { formatDate } from "../../../utils";
+import xlsx from "json-as-xlsx";
 
 export default function Rekap() {
     const [isLoading, setIsLoading] = useState(false)
@@ -34,6 +37,43 @@ export default function Rekap() {
         fetchUsers()
     },[fetchUsers])
 
+    function handleDownload() {
+        const columns = [
+            { label: "id", value: "id" },
+            { label: "nik", value: (row) => row.nik },
+            { label: "nama", value: "nama" },
+            { label: "tempatLahir", value: "tempatLahir" },
+            { label: "tanggalLahir", value: (row) => formatDate(row.tanggalLahir)},
+            { label: "jenisKelamin", value: "jenisKelamin" },
+            { label: "golonganDarah", value: "golonganDarah" },
+            { label: "alamat", value: "alamat" },
+            { label: "rt", value: "rt" },
+            { label: "rw", value: "rw" },
+            { label: "kelurahanDesa", value: "kelurahanDesa" },
+            { label: "kecamatan", value: "kecamatan" },
+            { label: "agama", value: "agama" },
+            { label: "statusPerkawinan", value: "statusPerkawinan" },
+            { label: "pekerjaan", value: "pekerjaan" },
+            { label: "kewarganegaraan", value: "kewarganegaraan" },
+            { label: "berlakuHingga", value: (row) => formatDate(row.berlakuHingga) },
+            { label: "pathFileKtp", value: "pathFileKtp" },
+            { label: "typeFileKtp", value: "typeFileKtp" }
+        ];
+        
+        const data = [{
+            sheet: 'pengguna',
+            columns,
+            content: users
+        }]
+
+        let settings = {
+            fileName: `Rekap-data-pemilih-${+new Date()}`,
+            extraLength: 3,
+        }
+        xlsx(data, settings)
+    }
+    
+
     return  <div className="flex flex-col h-full gap-2">
         <div className="stats shadow">
             <div className="stat">
@@ -45,10 +85,14 @@ export default function Rekap() {
                     {users && users.length}
                     {fetchError && <span className="bg-error px-2">Err</span>}
                 </div>
-                <div className="stat-desc">
+                <div className="stat-desc flex gap-2">
                     <button className="btn btn-outline btn-primary flex items-center gap-2" onClick={fetchUsers}>
                         {isLoading ? <AiOutlineLoading3Quarters className='text-2xl animate-spin'/> : <HiRefresh className='text-2xl'/>}
                         Segarkan
+                    </button>
+                    <button className="btn btn-outline btn-primary flex items-center gap-2" onClick={handleDownload}>
+                        <PiMicrosoftExcelLogo className="text-2xl"/>
+                        Download
                     </button>
                 </div>
             </div>
