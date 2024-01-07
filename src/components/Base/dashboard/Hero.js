35b0fba08b673a9b7874433577afd0f99dcf6bfa
McWooden <garryf7887@gmail.com>
2024-01-07 20:56:08 +0700

    memperbarui konten dashboard dan navbar

--- src/components/Base/dashboard/Hero.js
+++ src/components/Base/dashboard/Hero.js
@@ -2,12 +2,10 @@ import SearchAndPlusButton from "../../Utils/SearchAndPlusButton";
 
 export default function Hero() {
     return <div className="hero bg-base-200 rounded-lg py-4">
-        <div className="hero-content text-center z-auto">
-            <div className="max-w-md">
-                <h1 className="text-5xl font-bold">DAPIL</h1>
-                <p className="py-6">MAGELANG TENGAH - 4. IRNA</p>
-                <SearchAndPlusButton className={'max-2-md'}/>
-            </div>
+        <div className="hero-content text-center max-w-lg w-full flex flex-col gap-0">
+            <h1 className="text-4xl font-bold">Selamat datang!</h1>
+            <p className="py-6">Dapil MAGELANG TENGAH - 4. IRNA</p>
+            <SearchAndPlusButton className={'max-2-md'}/>
         </div>
     </div>
 }
